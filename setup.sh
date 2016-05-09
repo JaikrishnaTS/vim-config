ba9e402bda4c55e1e1cfd8c9515490a60cfa0b0d
Jaikrishna <t.s.jaikrishna@gmail.com>
2016-05-09 17:09:26 -0400

    modify wget arg

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 wget https://raw.githubusercontent.com/JaikrishnaTS/vim-config/master/.vimrc
 mkdir -p .vim/colors
-wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -O $HOME/.vim/colors
+wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -O $HOME/.vim/colors/monokai.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 rm -- "$0"
