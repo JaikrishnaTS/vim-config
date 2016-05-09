495a3259f33a35f15abbc26d855e13f94b66fa16
Jaikrishna <t.s.jaikrishna@gmail.com>
2016-05-09 17:07:35 -0400

    modified setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cp -r .vim $HOME
-cp .vimrc $HOME
+wget https://raw.githubusercontent.com/JaikrishnaTS/vim-config/master/.vimrc
+mkdir -p .vim/colors
+wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -O $HOME/.vim/colors
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+rm -- "$0"
