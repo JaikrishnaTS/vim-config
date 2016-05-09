4d220b60f766abfbb976eae4c7df652ce68a1a35
Jaikrishna <t.s.jaikrishna@gmail.com>
2016-05-09 17:27:15 -0400

    removed rm /bin/bash

--- setup.sh
+++ setup.sh
@@ -5,4 +5,3 @@ mkdir -p .vim/colors
 wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -O $HOME/.vim/colors/monokai.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-rm -- "$0"
