d8a34a80d795a662e8e0376b6fcc3b687187f8e5
Jaikrishna <t.s.jaikrishna@gmail.com>
2016-05-10 21:44:22 -0400

    vim input not from terminal fix

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ wget https://raw.githubusercontent.com/JaikrishnaTS/vim-config/master/.vimrc -O 
 mkdir -p .vim/colors
 wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -O $HOME/.vim/colors/monokai.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+vim - +PluginInstall +qall
