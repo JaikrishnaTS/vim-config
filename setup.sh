f28656d82c7fe7348c67894fc6e6fe43ad4ce39e
Jaikrishna <t.s.jaikrishna@gmail.com>
2016-06-03 12:36:51 -0400

    Fix with /home/jaikrishna

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/JaikrishnaTS/vim-config/master/.vimrc -O $HOME/.vimrc
-mkdir -p .vim/colors
+mkdir -p $HOME/.vim/colors
 wget https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim -O $HOME/.vim/colors/monokai.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim - +PluginInstall +qall
